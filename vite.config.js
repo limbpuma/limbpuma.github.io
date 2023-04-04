b2787a4ad90f3bc7af926fc99a10d7f551e8008f
Limber Martinez <limbpuma_de@hotmail.com>
2023-04-04 21:33:30 +0200

    deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import ghPages from 'vite-plugin-gh-pages';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ghPages()],
-});
+  base: '/web/',
+  plugins: [react()]
+})
