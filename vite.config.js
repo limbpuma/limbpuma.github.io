06e4d16808ae5b3919defcf587d6c30903677afe
Limber Martinez <limbpuma_de@hotmail.com>
2023-04-04 21:27:32 +0200

    changed deplyment to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import ghPages from 'vite-plugin-gh-pages';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/web/',
-  plugins: [react()]
-})
+  plugins: [ghPages()],
+});
